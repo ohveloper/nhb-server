4418986f48a8c03f91f1a9e73f160cb4086ad0f7
sangrae-cho <tkdfo93@gamil.com>
2021-04-09 17:22:01 +0900

    Feat#9: modified feed router

--- src/routers/feed.ts
+++ src/routers/feed.ts
@@ -1,9 +1,32 @@
 import { Router, Request, Response, NextFunction } from 'express';
+const { 
+  upload, 
+  bring, 
+  like, 
+  rank, 
+  remove, 
+  edit, 
+  cmtUpload,
+  cmtLike,
+  cmtRemove,
+  cmtEdit,
+  cmtBring
+} = require('../controllers/feed');
 
 const router = Router();
-
-router.get('/', (req:Request, res:Response, next: NextFunction) => {
-  res.send('this is /feed router');
-})
+//? 피드 관련 라우터
+router.post('/', upload);
+router.post('/lookup', bring);
+router.post('/like', like);
+router.delete('/', remove);
+router.patch('/', edit);
+//? 랭크 관련 라우터
+router.get('/rank', rank);
+//? 코멘트 관련 라우터
+router.post('/comment/lookup', cmtBring);
+router.post('/comment', cmtUpload);
+router.post('/comment/like', cmtLike);
+router.delete('/comment', cmtRemove);
+router.patch('/comment', cmtEdit);
 
 export default router;
