d753150b28a07beb969db2862a3bafc0624cad73
sangrae-cho <tkdfo93@gamil.com>
2021-04-12 21:57:33 +0900

    Feat#24: model set router

--- src/controllers/main/index.ts
+++ src/controllers/main/index.ts
@@ -2,11 +2,13 @@ import authEmail from './authEmail';
 import signUp from './signUp';
 import login from './login';
 import refreshToken from './refreshtoken';
+import logout from './logout'
 
 // TODO: 이메일인증/ 회원가입/ 로그인/ OAuth 등 메인 관련 메소드를 파일로 나누어 따로 만든 후 임포트
 module.exports = {
   authEmail,
   signUp,
   login,
-  refreshToken
+  refreshToken,
+  logout
 }
--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-const { authEmail, signUp, login, refreshToken } = require('../controllers/main');
+const { authEmail, signUp, login, refreshToken, logout } = require('../controllers/main');
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.post('/authemail', authEmail);
 router.post('/signup', signUp);
 router.post('/login', login);
 router.get('/refreshtoken', refreshToken);
+router.get('/logout', logout);
 
 export default router;
