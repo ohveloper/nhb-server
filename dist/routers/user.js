c078b01b0222ed030a685434123561e2d80f127e
sangrae-cho <tkdfo93@gamil.com>
2021-04-13 12:21:15 +0900

    Feat#37: get rid of user feeds func , do not need it more

--- dist/routers/user.js
+++ dist/routers/user.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const { bring, edit, withdrawal, feeds } = require('../controllers/user');
+const { bring, edit, withdrawal } = require('../controllers/user');
 const router = express_1.Router();
 router.post('/', bring);
 router.patch('/', edit);
 router.delete('/', withdrawal);
-router.post('/feed', feeds);
 exports.default = router;
--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,9 +1,7 @@
 import userHandler from './user';
-import bringPrivateFeeds from './feed';
 
 module.exports = {
   bring: userHandler.bring,
   edit: userHandler.edit,
   withdrawal: userHandler.withdrawal,
-  feeds: bringPrivateFeeds,
 };
