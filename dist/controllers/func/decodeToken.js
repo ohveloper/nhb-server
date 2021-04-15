908f00b2dbaba02434f9fdcda3ebdd627fff486a
sangrae-cho <tkdfo93@gamil.com>
2021-04-15 10:38:02 +0900

    Feat#39: decode token func

---
+++ dist/controllers/func/decodeToken.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.decodeToken = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const decodeToken = async (req, res) => {
+    const { authorization } = req.headers;
+    if (!authorization)
+        return { userId: null, message: 'unauthorized' };
+    const accessToken = authorization.split(' ')[1];
+    const accTokenSecret = process.env.ACCTOKEN_SECRET || 'acctest';
+    return await jsonwebtoken_1.default.verify(accessToken, accTokenSecret, (err, decoded) => {
+        if (err)
+            return { userId: null, message: 'invalid token' };
+        else
+            return { userId: decoded.id, message: null };
+    });
+};
+exports.decodeToken = decodeToken;
