af84dc00ee4a16c2bf82aac60386b5b188175a19
sangrae-cho <tkdfo93@gamil.com>
2021-04-18 09:53:04 +0900

    Feat#61: user tag edit

--- dist/controllers/user/tag.js
+++ dist/controllers/user/tag.js
@@ -1,8 +1,46 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tag_1 = require("../../models/tag");
-const tag = async (req, res, next) => {
-    const tags = await tag_1.Tags.findAll({ attributes: ['id', 'tagName', 'description', 'tagUrl'], raw: true });
-    res.status(200).json({ data: { tags }, message: 'All tags info' });
+const users_tag_1 = require("../../models/users_tag");
+const decodeToken_1 = require("../func/decodeToken");
+const tagHandler = {
+    bring: async (req, res, next) => {
+        const tags = await tag_1.Tags.findAll({ attributes: ['id', 'tagName', 'description', 'tagUrl'], raw: true });
+        res.status(200).json({ data: { tags }, message: 'All tags info' });
+    },
+    edit: async (req, res, next) => {
+        const { userId, message } = await decodeToken_1.decodeToken(req, res);
+        const { tagId } = req.body;
+        if (!userId)
+            return res.status(401).json({ message });
+        if (!tagId)
+            return res.status(400).json({ message: 'Need accurate informations' });
+        const tagInfo = await users_tag_1.Users_tags.findAll({ where: { userId }, attributes: ['tagId', 'isUsed'], raw: true });
+        let isAlreadyUsed = false;
+        let usedTag = null;
+        for (let i = 0; i < tagInfo.length; i += 1) {
+            if (tagInfo[i].isUsed === 1) {
+                usedTag = tagInfo[i].tagId;
+                if (tagInfo[i].tagId === tagId) {
+                    isAlreadyUsed = true;
+                }
+                break;
+            }
+        }
+        ;
+        if (isAlreadyUsed) {
+            //? 만약 이미 쓰는 뱃지를 눌렀다면 뱃지 표시 안하기
+            await users_tag_1.Users_tags.update({ isUsed: 0 }, { where: { userId, tagId } }).then(d => {
+                res.status(200).json({ data: { tagId: null }, message: "Detached tag" });
+            });
+        }
+        else {
+            //? 쓰는 뱃지가 아니라면 뱃지 교환 후 표시 하기
+            await users_tag_1.Users_tags.update({ isUsed: 0 }, { where: { userId, isUsed: 1 } });
+            await users_tag_1.Users_tags.update({ isUsed: 1 }, { where: { userId, tagId } }).then(d => {
+                res.status(200).json({ data: { tagId }, message: `Changed tag ${usedTag} to ${tagId}` });
+            });
+        }
+    }
 };
-exports.default = tag;
+exports.default = tagHandler;
--- src/controllers/user/tag.ts
+++ src/controllers/user/tag.ts
@@ -1,9 +1,46 @@
 import { Request, Response, NextFunction } from 'express';
 import { Tags } from '../../models/tag';
+import { Users_tags } from '../../models/users_tag';
+import { decodeToken } from '../func/decodeToken';
 
-const tag = async (req: Request, res: Response, next: NextFunction) => {
-  const tags = await Tags.findAll({attributes: ['id', 'tagName', 'description', 'tagUrl'], raw: true});
-  res.status(200).json({data: {tags}, message: 'All tags info'});
-};
+const tagHandler = {
+  bring: async (req: Request, res: Response, next: NextFunction) => {
+    const tags = await Tags.findAll({attributes: ['id', 'tagName', 'description', 'tagUrl'], raw: true});
+    res.status(200).json({data: {tags}, message: 'All tags info'});
+  },
 
-export default tag;
+  edit: async (req: Request, res: Response, next: NextFunction) => {
+    const { userId, message } = await decodeToken(req, res);
+    const { tagId } = req.body;
+    if (!userId) return res.status(401).json({message});
+    if (!tagId) return res.status(400).json({message: 'Need accurate informations'});
+    const tagInfo = await Users_tags.findAll({where: { userId }, attributes: ['tagId', 'isUsed'], raw: true});
+    let isAlreadyUsed = false;
+    let usedTag: null | number = null;
+    for (let i = 0; i < tagInfo.length; i += 1) {
+      if (tagInfo[i].isUsed === 1) {
+        usedTag = tagInfo[i].tagId;
+        if (tagInfo[i].tagId === tagId) {
+          isAlreadyUsed = true;
+        }
+        break;
+      }
+    };
+    
+    if (isAlreadyUsed) {
+      //? 만약 이미 쓰는 뱃지를 눌렀다면 뱃지 표시 안하기
+      await Users_tags.update({isUsed: 0}, {where: {userId, tagId}}).then(d => {
+        res.status(200).json({data: {tagId: null}, message: "Detached tag"});
+      });
+    } else {
+      //? 쓰는 뱃지가 아니라면 뱃지 교환 후 표시 하기
+      await Users_tags.update({isUsed: 0}, {where: {userId, isUsed: 1}});
+      await Users_tags.update({isUsed: 1}, {where: {userId, tagId}}).then(d => {
+        res.status(200).json({data: {tagId}, message: `Changed tag ${usedTag} to ${tagId}`});
+      });
+    }
+
+  }
+}
+
+export default tagHandler;
